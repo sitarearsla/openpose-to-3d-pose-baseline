0340745ab7432994e9960f821297fa7062cca811
Sitare Arslantürk <60389066+sitarearsla@users.noreply.github.com>
2022-01-21 00:33:25 +0300

    Update openpose_to_3dpose.py

--- src/openpose_to_3dpose.py
+++ src/openpose_to_3dpose.py
@@ -1,19 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import tensorflow as tf
 import data_utils
-import viz
 import re
 import cameras
 import json
 import os
 from predict_3dpose import create_model
-import imageio
-import logging
-from scipy.interpolate import UnivariateSpline
 
 FLAGS = tf.app.flags.FLAGS
+keypoint_order = [15, 12, 25, 26, 27, 17, 18, 19, 1, 2, 3, 6, 7, 8]
 
 
 def read_json():
@@ -82,6 +77,7 @@ def read_json():
         xy = _xy
         return xy
 
+
 def predict_depth(keypoints):
     enc_in = np.zeros((1, 64))
     enc_in[0] = [0 for i in range(64)]
@@ -93,9 +89,9 @@ def predict_depth(keypoints):
     rcams = cameras.load_cameras(os.path.join(this_file, "..", FLAGS.cameras_path), SUBJECT_IDS)
 
     train_set_2d, test_set_2d, data_mean_2d, data_std_2d, dim_to_ignore_2d, dim_to_use_2d = data_utils.create_2d_data(
-            actions, FLAGS.data_dir, rcams, 2)
+        actions, FLAGS.data_dir, rcams, 2)
     train_set_3d, test_set_3d, data_mean_3d, data_std_3d, dim_to_ignore_3d, dim_to_use_3d, train_root_positions, test_root_positions = data_utils.read_3d_data(
-            actions, FLAGS.data_dir, FLAGS.camera_frame, rcams, FLAGS.predict_14)
+        actions, FLAGS.data_dir, FLAGS.camera_frame, rcams, FLAGS.predict_14)
 
     device_count = {"GPU": 1}
     before_pose = None
@@ -107,13 +103,88 @@ def predict_depth(keypoints):
         iter_range = len(keypoints.keys())
         export_units = {}
         for n, (frame, xy) in enumerate(keypoints.items()):
+            print("Frame {0} / {1}".format(frame, iter_range))
             joints_array = np.zeros((1, 36))
             joints_array[0] = [0 for i in range(36)]
+            for i in range(len(joints_array[0])):
+                joints_array[0][i] = float(xy[i])
+            _data = joints_array[0]
+            for i in range(len(keypoint_order)):
+                for j in range(2):
+                    enc_in[0][keypoint_order[i] * 2 + j] = _data[i * 2 + j]
+            for j in range(2):
+                # Hip
+                enc_in[0][0 * 2 + j] = (enc_in[0][1 * 2 + j] + enc_in[0][6 * 2 + j]) / 2
+                # Neck/Nose
+                enc_in[0][14 * 2 + j] = (enc_in[0][15 * 2 + j] + enc_in[0][12 * 2 + j]) / 2
+                # Thorax
+                enc_in[0][13 * 2 + j] = 2 * enc_in[0][12 * 2 + j] - enc_in[0][14 * 2 + j]
+            # set spine
+            spine_x = enc_in[0][24]
+            spine_y = enc_in[0][25]
+
+            enc_in = enc_in[:, dim_to_use_2d]
+            mu = data_mean_2d[dim_to_use_2d]
+            stddev = data_std_2d[dim_to_use_2d]
+            enc_in = np.divide((enc_in - mu), stddev)
+
+            dp = 1.0
+            dec_out = np.zeros((1, 48))
+            dec_out[0] = [0 for i in range(48)]
+            _, _, poses3d = model.step(session, enc_in, dec_out, dp, isTraining=False)
+            all_poses_3d = []
+            enc_in = data_utils.unNormalizeData(enc_in, data_mean_2d, data_std_2d, dim_to_ignore_2d)
+            poses3d = data_utils.unNormalizeData(poses3d, data_mean_3d, data_std_3d, dim_to_ignore_3d)
+            all_poses_3d.append(poses3d)
+            enc_in, poses3d = map(np.vstack, [enc_in, all_poses_3d])
+            _max = 0
+            _min = 10000
+
+            for i in range(poses3d.shape[0]):
+                for j in range(32):
+                    tmp = poses3d[i][j * 3 + 2]
+                    poses3d[i][j * 3 + 2] = poses3d[i][j * 3 + 1]
+                    poses3d[i][j * 3 + 1] = tmp
+                    if poses3d[i][j * 3 + 2] > _max:
+                        _max = poses3d[i][j * 3 + 2]
+                    if poses3d[i][j * 3 + 2] < _min:
+                        _min = poses3d[i][j * 3 + 2]
+
+            for i in range(poses3d.shape[0]):
+                for j in range(32):
+                    poses3d[i][j * 3 + 2] = _max - poses3d[i][j * 3 + 2] + _min
+                    poses3d[i][j * 3] += (spine_x - 630)
+                    poses3d[i][j * 3 + 2] += (500 - spine_y)
+
+            if np.min(poses3d) < -1000:
+                poses3d = before_pose
+            p3d = poses3d
+            x, y, z = [[] for _ in range(3)]
+            if not poses3d is None:
+                to_export = poses3d.tolist()[0]
+            else:
+                to_export = [0.0 for _ in range(96)]
+            for i in range(0, len(to_export), 3):
+                x.append(to_export[i])
+                y.append(to_export[i + 1])
+                z.append(to_export[i + 2])
+
+            export_units[frame] = {}
+            for jnt_index, (_x, _y, _z) in enumerate(zip(x, y, z)):
+                export_units[frame][jnt_index] = {"translate": [_x, _y, _z]}
+
+            before_pose = poses3d
+
+    _out_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'maya/3d_data.json')
+    with open(_out_file, 'w') as outfile:
+        json.dump(export_units, outfile)
+        print("exported json to {0}".format(_out_file))
+        print("!!!!! DONE !!!!!")
 
 
 def main():
-    openpose_json = read_json()
-    predict_depth(openpose_json)
+    openpose_keypoints = read_json()
+    predict_depth(openpose_keypoints)
 
 
 if __name__ == "__main__":
